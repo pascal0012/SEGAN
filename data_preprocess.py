9d6a266262d956f76c28eaad50c63c7fda44b918
Pascal Makossa <pascal.makossa@tu-dortmund.de>
2024-09-18 09:53:22 +0200

    Made data path seperate env variable

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 import librosa
 import numpy as np
@@ -7,13 +7,14 @@ from tqdm import tqdm
 
 load_dotenv()
 
-clean_train_folder = os.getenv('CLEAN_TRAIN_FOLDER')
-noisy_train_folder = os.getenv('NOISY_TRAIN_FOLDER')
-clean_test_folder = os.getenv('CLEAN_TEST_FOLDER')
-noisy_test_folder = os.getenv('NOISY_TEST_FOLDER')
-serialized_train_folder = os.getenv('SERIALIZED_TRAIN_FOLDER')
-serialized_test_folder = os.getenv
-window_size = 2 ** 14  # about 1 second of samples
+data_path = os.getenv("DATA_PATH")
+clean_train_folder = os.path.join(data_path, os.getenv("CLEAN_TRAIN_FOLDER"))
+noisy_train_folder = os.path.join(data_path, os.getenv("NOISY_TRAIN_FOLDER"))
+clean_test_folder = os.path.join(data_path, os.getenv("CLEAN_TEST_FOLDER"))
+noisy_test_folder = os.path.join(data_path, os.getenv("NOISY_TEST_FOLDER"))
+serialized_train_folder = os.path.join(data_path, os.getenv("SERIALIZED_TRAIN_FOLDER"))
+serialized_test_folder = os.path.join(data_path, os.getenv("SERIALIZED_TEST_FOLDER"))
+window_size = 2**14  # about 1 second of samples
 sample_rate = 16000
 
 
@@ -38,7 +39,7 @@ def process_and_serialize(data_type):
     """
     stride = 0.5
 
-    if data_type == 'train':
+    if data_type == "train":
         clean_folder = clean_train_folder
         noisy_folder = noisy_train_folder
         serialized_folder = serialized_train_folder
@@ -53,7 +54,9 @@ def process_and_serialize(data_type):
     for root, dirs, files in os.walk(clean_folder):
         if len(files) == 0:
             continue
-        for filename in tqdm(files, desc='Serialize and down-sample {} audios'.format(data_type)):
+        for filename in tqdm(
+            files, desc="Serialize and down-sample {} audios".format(data_type)
+        ):
             clean_file = os.path.join(clean_folder, filename)
             noisy_file = os.path.join(noisy_folder, filename)
             # slice both clean signal and noisy signal
@@ -63,28 +66,37 @@ def process_and_serialize(data_type):
             # ex) p293_154.wav_5.npy denotes 5th slice of p293_154.wav file
             for idx, slice_tuple in enumerate(zip(clean_sliced, noisy_sliced)):
                 pair = np.array([slice_tuple[0], slice_tuple[1]])
-                np.save(os.path.join(serialized_folder, '{}_{}'.format(filename, idx)), arr=pair)
+                np.save(
+                    os.path.join(serialized_folder, "{}_{}".format(filename, idx)),
+                    arr=pair,
+                )
 
 
 def data_verify(data_type):
     """
     Verifies the length of each data after pre-process.
     """
-    if data_type == 'train':
+    if data_type == "train":
         serialized_folder = serialized_train_folder
     else:
         serialized_folder = serialized_test_folder
 
     for root, dirs, files in os.walk(serialized_folder):
-        for filename in tqdm(files, desc='Verify serialized {} audios'.format(data_type)):
+        for filename in tqdm(
+            files, desc="Verify serialized {} audios".format(data_type)
+        ):
             data_pair = np.load(os.path.join(root, filename))
             if data_pair.shape[1] != window_size:
-                print('Snippet length not {} : {} instead'.format(window_size, data_pair.shape[1]))
+                print(
+                    "Snippet length not {} : {} instead".format(
+                        window_size, data_pair.shape[1]
+                    )
+                )
                 break
 
 
-if __name__ == '__main__':
-    process_and_serialize('train')
-    data_verify('train')
-    process_and_serialize('test')
-    data_verify('test')
+if __name__ == "__main__":
+    process_and_serialize("train")
+    data_verify("train")
+    process_and_serialize("test")
+    data_verify("test")
